a5619a03653e7213c886f401f2a3fa460b89f187
0xEbrahim <elsayedebrahim603@gmail.com>
2025-08-28 12:25:45 +0300

    add support for Flushing and loading with multi-support for list & KV & hashs

--- DB/Database.go
+++ DB/Database.go
@@ -11,7 +11,10 @@ import (
 
 type Database struct {
 	Aof  *os.File
-	data map[string]string
+	kv   map[string]string
+	list map[string][]string
+	hash map[string]map[string]string
+	mtx  *sync.Mutex
 }
 
 var instance *Database
@@ -23,13 +26,14 @@ func GetDBInstance() *Database {
 		if err != nil || file == nil {
 			log.Fatal("Unable to open AOF")
 		}
-		instance = &Database{Aof: file, data: map[string]string{}}
+		instance = &Database{Aof: file, kv: map[string]string{}, list: map[string][]string{}, hash: map[string]map[string]string{}}
 
 	})
 	return instance
 }
 
 func (db *Database) Flush() {
+	db.mtx.Lock()
 	println("Flush")
 	err := db.Aof.Truncate(0)
 	if err != nil {
@@ -37,35 +41,82 @@ func (db *Database) Flush() {
 		return
 	}
 	db.Aof.Seek(0, 0)
-	data := db.data
 	defer db.Aof.Close()
-	for k, v := range data {
+	for k, v := range db.kv {
 		str := "SET " + k + " " + (v) + "\n"
-
 		_, err := db.Aof.Write([]byte(str))
-
 		if err != nil {
 			println("Error while writing AOF", err.Error())
 			return
 		}
 	}
+	for k, v := range db.list {
+		_, err := db.Aof.Write([]byte("LPUSH"))
+		if err != nil {
+			println("Error while writing AOF", err.Error())
+			return
+		}
+		_, err = db.Aof.Write([]byte(" " + k))
+		if err != nil {
+			println("Error while writing AOF", err.Error())
+			return
+		}
+		for _, it := range v {
+			_, err = db.Aof.Write([]byte(" " + it))
+			if err != nil {
+				println("Error while writing AOF", err.Error())
+				return
+			}
+		}
+		_, err = db.Aof.Write([]byte("\n"))
+		if err != nil {
+			println("Error while writing AOF", err.Error())
+			return
+		}
+	}
+	for k, v := range db.hash {
+		for K, V := range v {
+			str := "HSET " + k + " " + K + " " + V + "\n"
+			_, err := db.Aof.Write([]byte(str))
+			if err != nil {
+				println("Error while writing AOF", err.Error())
+				return
+			}
+		}
+	}
+	db.mtx.Unlock()
 }
 func (db *Database) Load() {
+	db.mtx.Lock()
 	println("LOAD")
 	sc := bufio.NewScanner(db.Aof)
 	for sc.Scan() {
 		line := sc.Text()
 		tokens := strings.Split(line, " ")
-		db.Set(tokens)
+		switch tokens[0] {
+		case "SET":
+			db.Set(tokens)
+		case "LPUSH":
+			db.Lpush(tokens)
+		case "HSET":
+			db.Hset(tokens)
+		default:
+
+		}
 	}
+	db.mtx.Unlock()
 }
 
 func (db *Database) Set(tokens []string) string {
-	db.data[tokens[1]] = tokens[2]
+	db.kv[tokens[1]] = tokens[2]
 	return "+OK\r\n"
 }
+func (db *Database) Lpush(token []string) {
+
+}
+func (db *Database) Hset(tokens []string) {}
 func (db *Database) Get(tokens []string) string {
-	str := db.data[tokens[1]]
+	str := db.kv[tokens[1]]
 	ret := "$" + strconv.Itoa(len(str)) + "\r\n" + str + "\r\n"
 	return ret
 }
